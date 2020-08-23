45840748e5063e3378e4a981720a1f0b5a6d7fdf
ReedyCloud <bartekciach1@gmail.com>
2020-08-23 21:05:18 +0100

    move from styled components to scss modules

--- src/components/Navbar/NavItem/NavItem.js
+++ src/components/Navbar/NavItem/NavItem.js
@@ -1,49 +1,16 @@
 import React from "react";
-import styled from "styled-components";
+import styles from "./NavItem.module.scss";
+import { classNames } from "../../../utils/helpers";
 
 const NavItem = ({ title, isActive }) => {
-  const StyledItem = styled.li`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    padding: 0.5rem 0;
-    color: #4aae9b;
-    cursor: pointer;
-    transition: background-color 150ms ease;
-
-    &:last-of-type {
-      margin-top: auto;
-    }
-
-    &:hover {
-      background-color: #073642;
-      svg {
-        color: #b58900;
-      }
-    }
-  `;
-
-  const StyledSpan = styled.span`
-    margin-left: 1.5rem;
-    font-size: 1.8rem;
-    flex-shrink: 0;
-    display: ${isActive ? "inilne" : "none"};
-  `;
-
-  const StyledSvg = styled.svg`
-    width: 5rem;
-    min-width: 5rem;
-    margin: 0 1.5rem;
-    transition: color 150ms ease;
-  `;
-
+  let NavTitleActive = isActive ? styles.NavTitleActive : null;
   return (
-    <StyledItem>
-      <StyledSvg
+    <li className={styles.NavItem}>
+      <svg
+        className={styles.NavSvg}
         aria-hidden="true"
         data-prefix="fas"
         data-icon="bowling-ball"
-        class="svg-inline--fa fa-bowling-ball fa-w-16"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 496 512"
       >
@@ -51,9 +18,13 @@ const NavItem = ({ title, isActive }) => {
           fill="currentColor"
           d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zM120 192c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm64-96c0-17.7 14.3-32 32-32s32 14.3 32 32-14.3 32-32 32-32-14.3-32-32zm48 144c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"
         />
-      </StyledSvg>
-      {title && <StyledSpan>{title}</StyledSpan>}
-    </StyledItem>
+      </svg>
+      {title && (
+        <span className={classNames(styles.NavTitle, NavTitleActive)}>
+          {title}
+        </span>
+      )}
+    </li>
   );
 };
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,40 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styles from "./Navbar.module.scss";
 
 import NavItem from "./NavItem/NavItem";
-
-const StyledNavbar = styled.div`
-  width: 8rem;
-  height: 100vh;
-  position: fixed;
-  background-color: #021e25;
-  transition: width 300ms ease;
-
-  &:hover {
-    width: 32rem;
-  }
-`;
-
-const StyledList = styled.ul`
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 100%;
-`;
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <StyledNavbar
+    <nav
+      className={styles.Navbar}
       onMouseEnter={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
     >
-      <StyledList>
+      <ul className={styles.NavList}>
+        {/* <li>
+          <span>Bartosz Ciach</span>
+          <svg
+            aria-hidden="true"
+            data-prefix="fas"
+            data-icon="angle-double-right"
+            class="svg-inline--fa fa-angle-double-right fa-w-14"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path
+              fill="currentColor"
+              d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"
+            />
+          </svg>
+        </li> */}
         <NavItem title="O Mnie" isActive={isOpen} />
         <NavItem title="Projekty" isActive={isOpen} />
+        <NavItem title="Umiejętności" isActive={isOpen} />
         <NavItem title="Kontakt" isActive={isOpen} />
-        <NavItem title="Tło" isActive={isOpen} />
-      </StyledList>
-    </StyledNavbar>
+      </ul>
+    </nav>
   );
 };
 
